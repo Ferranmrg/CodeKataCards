cb96148d11fbbc15b5cceea4d292f1726c639db5
Ferran Martinez <ferranmrg@gmail.com>
2018-03-02 15:32:17 +0100

    created player component

--- test/game.spec.js
+++ test/game.spec.js
@@ -4,12 +4,19 @@ let chai = require('chai'),
 chai.should();
 
 let Game = require(path.join(__dirname, '..', 'game'));
+let Player = require(path.join(__dirname, '..', 'player'));
 
 describe('Game', () => {
   describe('#constructor()', () => {
     it('requires two players', () => {
       () => {
         new Game();
+      }.should.throw(Error);
+
+      () => {
+        let Player1 = new Player();
+        let Player2 = new Player();
+        new Game(Player1, Player2);
       }.should.throw(Error);
 
     });
